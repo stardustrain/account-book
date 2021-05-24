9e589a0f88c5f0d2ea7b745d5ca57aba341734f9
stardustrain <kthanterran@gmail.com>
2021-05-24 21:56:58 +0900

    Issue-17 Add mercurisu types

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -4,6 +4,8 @@ import mercurius from 'mercurius'
 import schema, { dataSources } from './graphql'
 import Prisma from './utils/Prisma'
 
+import type { Loaders } from './utils/types'
+
 const server = fastify({
   // logger: true,
 })
@@ -16,6 +18,10 @@ const contextBuilder = async () => {
 }
 type PromiseType<T> = T extends PromiseLike<infer U> ? U : T
 export type Context = PromiseType<ReturnType<typeof contextBuilder>>
+declare module 'mercurius' {
+  interface MercuriusContext extends Context {}
+  interface MercuriusLoaders extends Loaders {}
+}
 
 server.register(mercurius, {
   schema,
