3ec72c372b3c632b827e692ea14002c395e08a4c
stardustrain <kthanterran@gmail.com>
2021-05-24 21:54:45 +0900

    Issue-17 Add log settings to Prisma

--- projects/server/src/utils/Prisma.ts
+++ projects/server/src/utils/Prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '../../generated/client'
 import type { Prisma as PrismaType } from '../../generated/client'
 
 const options: PrismaType.PrismaClientOptions = {
+  log: ['query'],
   rejectOnNotFound: () => {
     throw Error('NOT_FOUND')
   },
@@ -13,6 +14,15 @@ export default class Prisma {
   static getInstance() {
     if (!this.client) {
       this.client = new PrismaClient(options)
+      this.client.$use(async (params, next) => {
+        const before = Date.now()
+        const result = await next(params)
+        const after = Date.now()
+
+        console.log(`Query ${params.model}.${params.action} => ${after - before}ms`)
+
+        return result
+      })
     }
 
     return this.client
