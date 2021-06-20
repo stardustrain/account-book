4da48852166cc74e702987b1cc602655049618e9
StellaKim1230 <stellakim1230@gmail.com>
2021-06-20 16:24:05 +0900

    Add stitiches setting.

---
+++ projects/webapp/styles/stitches.config.ts
@@ -0,0 +1,28 @@
+import { createCss } from '@stitches/react'
+
+export const { styled, global, keyframes, getCssString, theme } = createCss({
+  theme: {
+    colors: {
+      black10: 'var(--colors-black-10)',
+      black20: 'var(--colors-black-20)',
+      black30: 'var(--colors-black-30)',
+      black40: 'var(--colors-black-40)',
+      black50: 'var(--colors-black-50)',
+      black60: 'var(--colors-black-60)',
+      black70: 'var(--colors-black-70)',
+      black80: 'var(--colors-black-80)',
+      black90: 'var(--colors-black-90)',
+      black100: 'var(--colors-black-100)',
+      blue10: 'var(--colors-blue-10)',
+      blue20: 'var(--colors-blue-20)',
+      blue30: 'var(--colors-blue-30)',
+      blue40: 'var(--colors-blue-40)',
+      blue50: 'var(--colors-blue-50)',
+      blue60: 'var(--colors-blue-60)',
+      blue70: 'var(--colors-blue-70)',
+      blue80: 'var(--colors-blue-80)',
+      blue90: 'var(--colors-blue-90)',
+      blue100: 'var(--colors-blue-100)',
+    },
+  },
+})
